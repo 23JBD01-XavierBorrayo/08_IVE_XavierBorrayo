536f2059d8074de81bb2057af3eb92fe359f6b2a
23JBD01-XavierBorrayo <gatunavi@gmail.com>
2026-03-16 12:18:24 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Entry
+        int num;
+        Console.Write("Give me whatever number");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        //Exit
+        if (num > 0)
+        {
+            Console.WriteLine("Im positive number");
+        }
+        else
+        {
+            Console.WriteLine("Im negative number");
+        }
+        Console.WriteLine("Negative and Positive Numbers");
+
+    }
+}
