5f346a21a3d9529472ae776d98b9c712a5c3c152
Bolayo <gatunavi@gmail.com>
2026-04-13 12:20:43 -0600

    Add files via upload

---
+++ G2CLASS3.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Menu AMERICAN RESTAURANT:");
+        int menu;
+        menu = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("1 -> Hamburguesa");
+        Console.WriteLine("2 -> Pizza");
+        Console.WriteLine("3 -> Pollo Frito");
+        Console.WriteLine("4 -> Ensalada");
+
+        
+
+        switch (menu)
+        {
+            case 1:
+                Console.WriteLine("Hamburguesa 50$");
+                break;
+            case 2:
+                Console.WriteLine("Pizza 35$");
+                break;
+            case 3:
+                Console.WriteLine("Pollo frito 40$");
+                break;
+            case 4:
+                Console.WriteLine("Ensalada 20$");
+                break;
+            default:
+                Console.WriteLine("Unicamente (1, 2, 3 y 4)");
+                break;
+        }
+
+    }
+}
