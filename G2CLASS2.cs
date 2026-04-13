c3f5f88539aa8a4216098220a122b2d535abb891
Bolayo <gatunavi@gmail.com>
2026-04-13 12:04:50 -0600

    Add files via upload

---
+++ G2CLASS2.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("RANKING TIER LIST:");
+
+        Console.WriteLine("Write a Key (A, B, C, D, F): ");
+        char key = Convert.ToChar(Convert.ToInt32(Console.ReadLine().ToUpper()));
+
+        switch (key)
+        {
+            case 'A':
+                Console.WriteLine("20 / 10 GOD");
+                break;
+            case 'B':
+                Console.WriteLine("Good Try");
+                break;
+            case 'C':
+                Console.WriteLine("NPC");
+                break;
+            case 'D':
+                Console.WriteLine("Trash or Garbage");
+                break;
+            case 'F':
+                Console.WriteLine("Get out!");
+                break;
+            default:
+                Console.WriteLine("Only this NUMBRES (A, B, C, D, F)");
+                break;
+        }
+
+    }
+}
---
+++ G2Class2.psc
@@ -0,0 +1,22 @@
+Algoritmo sin_titulo
+	Escribir "CLASIFICACIÓN DE LETRAS POR RANGO"
+	//VARIABLE
+	Definir l Como Caracter
+	Escribir "Digite una letra: " sin saltar
+	leer l
+	
+	Segun l Hacer
+		"A" o "a":
+			Escribir "EXCELSIOR"
+		"B" o "b":
+			Escribir "BUENO"
+		"C" o "c":
+			Escribir "REGULAR"
+		"D" o "d":
+			Escribir "DEFICIENTE"
+		"F" o "f":
+			Escribir "REPROBADO"
+		De Otro Modo:
+			Escribir "Digite letra valida (A, B, C, D y F)"
+	Fin Segun
+FinAlgoritmo
