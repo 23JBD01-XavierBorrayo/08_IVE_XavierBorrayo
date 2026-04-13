d423ca14f16abd724966bbdfe17583bb3589c4e0
Bolayo <gatunavi@gmail.com>
2026-04-13 11:49:33 -0600

    Add files via upload

---
+++ G2Class.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("WEEK DAYS:");
+
+        int day;
+        Console.WriteLine("Write a number of 1 to 7");
+        day = Convert.ToInt32(Console.ReadLine());
+
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("MONDAY");
+                break;
+            case 2:
+                Console.WriteLine("THUESDAY");
+                break;
+            case 3:
+                Console.WriteLine("WEDNESDAY");
+                break;
+            case 4:
+                Console.WriteLine("THURSTDAY");
+                break;
+            case 5:
+                Console.WriteLine("FRIDAY");
+                break;
+            case 6:
+                Console.WriteLine("SATURDAY");
+                break;
+            case 7:
+                Console.WriteLine("SUNDAY");
+                break;
+            default:
+                Console.WriteLine("Write a value number of 1 to 7");
+                break;
+        }
+
+    } 
+}
---
+++ G2Class.psc
@@ -0,0 +1,26 @@
+Algoritmo sin_titulo
+	Escribir "Días de la semana"
+	//VARIABLE
+	Definir dia Como Entero
+	Escribir "Digite un valor numerico entre el 1 y el 7"
+	leer dia
+	
+	Segun dia Hacer
+		1:
+			Escribir "Lunes"
+		2:
+			Escribir "Martes"
+		3:
+			Escribir "Miercoles"
+		4:
+			Escribir "Jueves"
+		5:
+			Escribir "Viernes"
+		6:
+			Escribir "Sabado"
+		7:
+			Escribir "Domingo"
+		De Otro Modo:
+			Escribir "Digite un valor valido del 1 al 7"
+	Fin Segun
+FinAlgoritmo
