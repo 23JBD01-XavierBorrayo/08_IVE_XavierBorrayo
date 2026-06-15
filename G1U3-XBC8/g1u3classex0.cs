d7cf3e7d3dd912cb1d36d2b8aa487bc241b3c947
Bolayo <gatunavi@gmail.com>
2026-06-15 12:15:05 -0600

    Add files via upload

---
+++ G1U3-XBC8/g1u3classex0.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("_-_-LECC2ACT2INC1-_-_");
+        Console.WriteLine("Xavier Borrayo / Clave 8");
+
+        int[] People_age = new int[7];
+        int cp = 0;
+
+        for (int i = 1; i < People_age.Length; i++)
+        {
+            Console.WriteLine("Write your age " + i + ": ");
+            People_age[i] = Convert.ToInt32(Console.ReadLine());
+            if (People_age[i] >= 18) ;
+            cp++;
+
+        }
+        Console.Clear();
+        Console.WriteLine("Las edades ingresadas son :");
+        for (int i = 0; i < People_age.Length; i++)
+        {
+            Console.WriteLine(People_age[i] + ": ");
+        }
+        Console.WriteLine("La cantidad de personas mayores de edad son: " + cp);
+    }
+}
